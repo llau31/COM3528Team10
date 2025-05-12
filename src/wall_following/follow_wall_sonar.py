48823ef28b02a8a57f8e9d24d70f0abda64537fd
{Luke Parry} <{lparry1@sheffield.ac.uk}>
2025-05-12 19:29:12 +0100

    Regular sonar distance ranges found, pushing so can be tested on robot too.

--- src/wall_following/follow_wall_sonar.py
+++ src/wall_following/follow_wall_sonar.py
@@ -14,11 +14,10 @@ class FollowWallSonar():
 
     # uses sonar to detect objects in front of nose
     def sonar_callback(self, sonar_data: Range):
-        self.front_range = sonar_data.range
-        current_time = time.time()
-        if current_time - self.start_time_2 > 1.8:
+        if self.region != self.prev_region:
+            self.front_range = sonar_data.range
             print(f"sonar reading: {self.front_range} m")
-            self.start_time_2 = current_time
+            self.printed = True
 
     def imu_callback(self, imu_data: Imu):
         x = imu_data.orientation.x
@@ -47,16 +46,27 @@ class FollowWallSonar():
 
         if 0.58 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < 0.62 and -0.82 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < -0.78:
             print("BOTTOM LEFT")
+            self.region = "BOTTOM LEFT"
         if 0.94 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < 1 and -0.16 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < -0.13:
             print("BOTTOM MIDDLE")
+            self.region = "BOTTOM MIDDLE"
         if 0.49 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < 0.55 and 0.81 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < 0.85:
             print("BOTTOM RIGHT")
+            self.region = "BOTTOM RIGHT"
         if -0.05 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < 0.05 and -1 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < -0.98:
             print("TOP LEFT")
+            self.region = "TOP LEFT"
         if -1 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < -0.93 and -0.05 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < 0.05:
             print("TOP MIDDLE")
+            self.region = "TOP MIDDLE"
         if -0.05 < math.sin(0.5 * current_time) and math.sin(0.5 * current_time) < 0.05 and 0.98 < math.cos(0.5 * current_time) and math.cos(0.5 * current_time) < 1:
             print("TOP RIGHT")
+            self.region = "TOP RIGHT"
+
+        if self.region != self.prev_region and self.printed:
+            self.prev_region = self.region
+            self.printed = False
+
         
         temp_joint_state.position = positions
         temp_joint_state.name = ["tilt", "lift", "yaw", "pitch"]
@@ -98,6 +108,9 @@ class FollowWallSonar():
         temp_joint_state.velocity = ()
         temp_joint_state.effort = ()
         self.move_head_pub.publish(temp_joint_state)
+        self.region = ""
+        self.prev_region = ""
+        self.printed = False
         
         self.is_break = False
         
