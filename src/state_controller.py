17ad18c24124f4c8cf5c6fa8ae1d5683581e0462
{Luke Parry} <{lparry1@sheffield.ac.uk}>
2025-05-14 09:14:55 +0100

    Edits to state controller.

--- src/state_controller.py
+++ src/state_controller.py
@@ -6,6 +6,7 @@ import os
 from sensor_msgs.msg import Range
 from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import Int32
+from std_msgs.msg import Bool
 
 # from roaming.random_walk import run_random_walk
 # from roaming.levy_walk import run_levy_walk
@@ -22,6 +23,8 @@ class StateController:
 
     # def callback_sensor(self, msg: Range):
     #     print(f'Distance: {msg.range} meters')
+    def wall_callback(self, wall: Bool):
+        self.wall_detected = wall.data
 
     # def callback_face(self, msg: bool):
     #     self.face_detected = msg
@@ -34,6 +37,7 @@ class StateController:
 
         # Publish to network
         self.pub = rospy.Publisher(topic_name, Int32, queue_size=10)
+        self.wall_sub = rospy.Subscriber('/wall_detected', Bool, self.wall_callback)
         rospy.init_node("state_controller", anonymous=True)
         rospy.sleep(2.0)
 
